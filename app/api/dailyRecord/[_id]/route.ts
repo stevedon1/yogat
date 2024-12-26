0ab6ba930d534f08041f0efbfeafb4fef6d91dc4
Steve <stevowny@gmail.com>
2024-12-26 15:00:49 +0300

    API for individual record

---
+++ app/api/dailyRecord/[_id]/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import connectToDatabase from '@/lib/mongodb';
+import { Types } from "mongoose";
+import DailyRecord from "@/models/DailyRecord";
+
+// GET /api/records/:id
+export async function GET(request: Request,{ params }: { params: { _id: string } }) {
+  const { _id } = params;
+
+  if (!Types.ObjectId.isValid(_id)) {
+    return NextResponse.json(
+      { error: "Invalid record ID" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    await connectToDatabase();
+    const record = await DailyRecord.findById(_id);
+
+    if (!record) {
+      return NextResponse.json(
+        { error: "Record not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ data: record }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to fetch the record" },
+      { status: 500 }
+    );
+  }
+}
